c1603cab18115d039edda7201f300d809e95bc4e
Amir <amirshoxnutfullayev@gmail.com>
2024-02-22 12:30:42 +0500

    added

---
+++ paginations/src/main/java/com/example/paginations/PaginationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.paginations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaginationsApplication.class, args);
+    }
+
+}
